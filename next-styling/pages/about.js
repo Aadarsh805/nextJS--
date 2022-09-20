0451581a301caf6603dd235c52650dda7aca3f34
aadarsh805 <aadarshthakur80501@gmail.com>
2022-09-20 16:38:46 +0530

    styling done

---
+++ next-styling/pages/about.js
@@ -0,0 +1,10 @@
+import styles from '../styles/About.module.scss'
+
+const about = () => {
+  return (
+    <div className={styles.highlightscss}>About</div>
+
+  )
+};
+
+export default about;
---
+++ next-styling/pages/contact.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Contact.module.scss'
+
+
+const contact = () => {
+  return (
+    <div className={styles.highlightscss}>Contact</div>
+  )
+}
+
+export default contact
